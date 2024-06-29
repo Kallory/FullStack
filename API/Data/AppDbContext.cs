3fcf199ac4a983ac6c16016ae98af191d62ac458
Kallory <kory232323@gmail.com>
2024-06-29 17:32:09 -0500

    Adding service to connect to DB

---
+++ API/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore; // library
+
+namespace API.Data // Namespaces are used to organize code into a hierarchical structure, making it easier to manage and avoid naming conflicts.
+{
+    public class AppDbContext:DbContext
+    {
+        // the :base(options) is like calling super from a derived class in Java
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options) // defines a public class named AppDbContext that inherits from DbContext. The DbContext class is a part of Entity Framework Core and serves as a bridge between your domain or entity classes and the database. It allows you to query and save data.
+        {
+            
+        }
+    }
+}
