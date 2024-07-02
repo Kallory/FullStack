460a4109782d6bd25ec9b3f765263a0c81d1e55e
Kallory <kory232323@gmail.com>
2024-07-01 21:40:25 -0500

    Implementing httpGet for products

---
+++ API/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+using API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController:ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ProductController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Products() 
+        {
+            return Ok(await _context.Products.AsNoTracking().ToListAsync());
+        }
+    }
+}
